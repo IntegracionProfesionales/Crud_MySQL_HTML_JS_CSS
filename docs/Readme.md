12206c5c5dd42d190fd9bf116178871706267f47
IrmaRamirez <iriraga@hotmail.com>
2021-10-03 21:15:04 -0500

    enrutamientos get/all post/all get/id put/damage

--- docs/Readme.md
+++ docs/Readme.md
@@ -3,6 +3,13 @@
 (https://youtu.be/9XewV95xGh0)
 
 ## Stacks
+* GitHub
 * VSCode
+* Vim :v
+* JavaScript
 * MySQL
 * NodeJS
+* npm
+* Express
+* Cors
+* Postman
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const db = require('../database/conexion');
+const express = require("express");
+const cors = require("cors");
+const db = require("../database/conexion");
 const app = express();
 
 // Settings
 
 //Middlewares
 // Acciones que se ejecutan antes de que se exploren las rutas
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 // Convertir de JSON a Variables:
 app.use(express.json());
 // Guardamos el puerto como una variable
@@ -15,31 +15,82 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
-
-
 // Servidor API REST
 // Routers: Enrutamiento según la petición hecha en la página Web.
 // PENDIENTE: Mover todas las peticiones al archivo:
 // app.use(require('./routes/movimiento'));
-app.get('/productos', (req, res)=>{
-    const consulta_sql = 'SELECT * FROM flujo';
-    
-    db.query(consulta_sql, (err, data)=>{
-        if(err){
-             return err;
-        } else {
-            res.json({
-                nombre_envia: "un nombre",
-                nombre_recibe: "otro nombre",
-                tipo_recurso: "un recurso",
-                cantidad: 4,
-                FechaHora: "2015-12-20 10:01:00",
-                data
-            });
-        }
-    });
+app.get("/all", (req, res) => {
+  const consulta_sql = "SELECT * FROM flujo";
+  db.query(consulta_sql, (err, data) => {
+    if (err) return err;
+    else res.json(data);
+  });
+});
+
+app.post("/all", (req, res) => {
+  const values = Object.values(req.body);
+  console.log(values);
+
+  const consulta_sql =
+    "INSERT INTO recursos.flujo (nombre_envia, nombre_recibe, tipo_recurso, cantidad, FechaHora) VALUES (?,?,?,?,?)";
+
+  db.query(consulta_sql, values, (err, result) => {
+    if (err) {
+      console.log("inserción fallida :(");
+      res.json({
+        "esto es un mensaje": "inserción fallida :'(",
+      });
+      return err;
+    } else {
+      console.log("inserción exitosa :)");
+      res.json({
+        "esto es un mensaje": "inserción exitosa",
+        result,
+      });
+    }
+  });
+});
+
+app.get("/:id", (req, res) => {
+  console.log(req.params.id);
+  const values = req.params.id;
+  const consulta_sql = "SELECT * FROM flujo WHERE idflujo = ?;";
+  db.query(consulta_sql, values, (err, data) => {
+    if (err) {
+      console.log("petición fallida");
+      return err;
+    } else {
+      res.json(data);
+      console.log("petición exitosa");
+    }
+  });
+});
+
+app.put("/gatos", (req, res) => {
+  const values = Object.values(req.body);
+  // const values = Object.values(req.body).concat(parseInt(req.params.id));
+  console.log(values);
+
+  const consulta_sql =
+    "UPDATE recursos.flujo SET nombre_envia=?, nombre_recibe=?, tipo_recurso=?, cantidad=?, FechaHora=? WHERE idflujo=?";
+
+  db.query(consulta_sql, values, (err, result) => {
+    if (err) {
+      console.log("edición fallida :(");
+      res.json({
+        "esto es un mensaje": "edición fallida :'(",
+      });
+      return err;
+    } else {
+      console.log("edición exitosa :)");
+      res.json({
+        "esto es un mensaje": "edición exitosa",
+        result,
+      });
+    }
+  });
 });
 
 app.listen(PORT, () => {
-    console.log('Servidor on Port: '+ PORT);
+  console.log("Servidor on Port: " + PORT);
 });
